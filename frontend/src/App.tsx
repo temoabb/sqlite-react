7618beffb62e7e4327622bb04f461b7971313e78
Temo Abesadze <abesadzetemo1997@gmail.com>
2025-01-07 17:36:00 +0400

    chore: install dependencies (lucide-react, fontsource/poppins), init shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-green-500">Todo App</h1>;
+  return (
+    <>
+      <h1 className="text-green-600 font-bold">Todo App</h1>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import App from "./App.tsx";
+
 import "./index.css";
 
-import App from "./App.tsx";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/700.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
